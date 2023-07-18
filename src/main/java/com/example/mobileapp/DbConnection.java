c8a58fd8343dec80eb70d391e795dcb41beea0f6
Martin' FixerFoxer' Genchev <martingenchev154@gmail.com>
2023-07-18 12:10:21 +0300

    changed dbConnection file, added getusers method in UserService

--- src/main/java/com/example/mobileapp/DbConnection.java
+++ src/main/java/com/example/mobileapp/DbConnection.java
@@ -3,31 +3,36 @@ package com.example.mobileapp;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Objects;
+@Component
+public class DbConnection {
 
-public class DbConnection{
+    @PostConstruct
+    public void dbConnection() {
+        if (FirebaseApp.getApps().isEmpty()) {
+            ClassLoader classLoader = getClass().getClassLoader();
 
-    public void dbConnection(){
-        ClassLoader classLoader = MobileAppApplication.class.getClassLoader();
+            try {
+                File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
 
-        try {
-            File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
+                FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
+                FirebaseOptions options = new FirebaseOptions.Builder()
+                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .setDatabaseUrl("https://mobile-dafb8-default-rtdb.europe-west1.firebasedatabase.app")
+                        .build();
 
-            FileInputStream serviceAccount =
-                    new FileInputStream(file.getAbsolutePath());
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .setDatabaseUrl("https://mobile-dafb8-default-rtdb.europe-west1.firebasedatabase.app")
-                    .build();
-
-            FirebaseApp.initializeApp(options);
-        }catch (IOException e){
-            e.printStackTrace();
+                FirebaseApp.initializeApp(options);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
+    // Rest of the class implementation
 }
--- src/main/java/com/example/mobileapp/user/UserService.java
+++ src/main/java/com/example/mobileapp/user/UserService.java
@@ -2,10 +2,7 @@ package com.example.mobileapp.user;
 
 import com.example.mobileapp.listing.Listing;
 import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.DocumentReference;
-import com.google.cloud.firestore.DocumentSnapshot;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.WriteResult;
+import com.google.cloud.firestore.*;
 import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.stereotype.Service;
 
@@ -19,26 +16,27 @@ public class UserService {
     public static final String COL_NAME="Users";
 
     public String addUser() throws InterruptedException, ExecutionException {
-        User user = new User(1L,"usernamedemo","passworddemo","emaildemo", "phonenumberdemo");
+        User user = new User(2L,"usernamedemo","passworddemo","emaildemo", "phonenumberdemo");
         Firestore dbFirestore = FirestoreClient.getFirestore();
         ApiFuture<WriteResult> collectionsApiFuture = dbFirestore.collection(COL_NAME).document(user.getId().toString()).set(user);
         return collectionsApiFuture.get().getUpdateTime().toString();
     }
-    public List<User> getAll(){
-        User user1 = new User(1L,
-                "username1",
-                "password1",
-                "user1@gmail.com",
-                "088321312");
-        User user2 = new User(2L,
-                "username2",
-                "password2",
-                "user2@gmail.com",
-                "088324482");
-        List<User> listUsers = new ArrayList<>();
-        listUsers.add(user1);
-        listUsers.add(user2);
-        return listUsers;
+    public List<User> getAll() throws ExecutionException, InterruptedException {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        CollectionReference usersCollection = dbFirestore.collection(COL_NAME);
+        ApiFuture<QuerySnapshot> future = usersCollection.get();
+
+        QuerySnapshot querySnapshot = future.get();
+        List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
+
+        List<User> userList = new ArrayList<>();
+
+        for (QueryDocumentSnapshot document : documents) {
+            User user = document.toObject(User.class);
+            userList.add(user);
+        }
+
+        return userList;
     }
 
 }
