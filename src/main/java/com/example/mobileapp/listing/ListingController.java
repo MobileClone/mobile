075a54177e9c150ecb97fff5732a698f889add37
FrenDo0 <lubo_raykonen@mail.bg>
2023-07-19 17:04:55 +0300

    added listings.html file to display all listings

--- src/main/java/com/example/mobileapp/listing/ListingController.java
+++ src/main/java/com/example/mobileapp/listing/ListingController.java
@@ -5,6 +5,7 @@ import com.example.mobileapp.user.User;
 import com.google.firebase.database.DataSnapshot;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-@RestController
+@Controller
 public class ListingController {
 
     private final ListingService listingService;
@@ -21,7 +22,7 @@ public class ListingController {
     public ListingController(ListingService listingService) {
        this.listingService = listingService;
     }
-    
+
     @GetMapping("/getPatientDetails")
     public Listing getListing(@RequestParam Long id ) throws InterruptedException, ExecutionException{
         return listingService.getListing(id);
@@ -59,4 +60,10 @@ public class ListingController {
         listingService.createListing(listing);
     }
 
+    @GetMapping (value = "/listings")
+    public String getAllListings(Model model) throws ExecutionException, InterruptedException {
+        List<Listing> listingList = listingService.getAllListings();
+        model.addAttribute("listings",listingList);
+        return "listings";
+    }
 }
